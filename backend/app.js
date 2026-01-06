f10e1a8629594acb48726703b5c02f3d65a81104
bitbunnydev <bitbunnydev@gmail.com>
2026-01-07 02:52:14 +0800

    User backend structure

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import quizRoutes from "./src/routes/quizRoutes.js";
+import noteRoutes from "./src/routes/noteRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 import cors from "cors";
 
 const app = express();
 
+//middleware
 app.use(express.json());
+//routes
 app.use("/api/quiz", quizRoutes);
+app.use("/api/note", noteRoutes);
+app.use("/api/user", authRoutes);
 
 export default app;
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,13 @@
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function register(req, res) {
+  try {
+  } catch (error) {}
+}
+
+export async function login(req, res) {
+  try {
+  } catch (error) {}
+}
